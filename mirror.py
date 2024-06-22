9e9faa6317b48f919018ad89952204cf4b3abf5f
detachhead <detachhead@users.noreply.github.com>
2024-06-22 11:20:49 +1000

    replace black with basedpyright

--- mirror.py
+++ mirror.py
@@ -12,18 +12,18 @@ def main():
     with open(Path(__file__).parent / "pyproject.toml", "rb") as f:
         pyproject = tomli.load(f)
 
-    # get current version of black
+    # get current version of basedpyright
     deps = pyproject["project"]["dependencies"]
     assert len(deps) == 1
-    black_dep = Requirement(deps[0])
-    assert black_dep.name == "black"
-    black_specs = list(black_dep.specifier)
-    assert len(black_specs) == 1
-    assert black_specs[0].operator == "=="
-    current_version = Version(black_specs[0].version)
+    basedpyright_dep = Requirement(deps[0])
+    assert basedpyright_dep.name == "basedpyright"
+    basedpyright_specs = list(basedpyright_dep.specifier)
+    assert len(basedpyright_specs) == 1
+    assert basedpyright_specs[0].operator == "=="
+    current_version = Version(basedpyright_specs[0].version)
 
-    # get all versions of black from PyPI
-    resp = urllib3.request("GET", "https://pypi.org/pypi/black/json")
+    # get all versions of basedpyright from PyPI
+    resp = urllib3.request("GET", "https://pypi.org/pypi/basedpyright/json")
     if resp.status != 200:
         raise RuntimeError
 
@@ -32,11 +32,11 @@ def main():
     versions.sort()
 
     for version in versions:
-        pyproject["project"]["dependencies"] = [f"black=={version}"]
+        pyproject["project"]["dependencies"] = [f"basedpyright=={version}"]
         with open(Path(__file__).parent / "pyproject.toml", "wb") as f:
             tomli_w.dump(pyproject, f)
         subprocess.run(["git", "add", "pyproject.toml"])
-        subprocess.run(["git", "commit", "-m", f"black {version}"])
+        subprocess.run(["git", "commit", "-m", f"basedpyright {version}"])
         subprocess.run(["git", "tag", f"{version}"])
 
 
